9bf5dd7e03ed1e3e4904fad1541aa8f1e0613477
ZGH1204 <972833647@qq.com>
2023-07-18 13:20:31 +0800

    1

---
+++ Runtime/Scripts/Common/Utils.Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZGH.Core
+{
+    public static partial class Utils
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var t in source) {
+                action(t);
+            }
+        }
+    }
+}
